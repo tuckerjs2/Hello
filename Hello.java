0e68aa1b1607eb2ac01274a0eaaed9184cdf39bf
tuckerjs2 <46756332+tuckerjs2@users.noreply.github.com>
2019-01-16 11:59:51 -0500

    Hello.java
    lab1

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/***************************************************************************
+* Java Program Name: Hello
+****************************************************************************
+* Project Description: Lab 1 
+*___________________________________________________________________________
+*Student First Name Last Name: Jeffrey Tucker
+*Date: 1/16/2019
+*Course Number and Section: CMSC 255 004
+****************************************************************************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Jeffrey Tucker");
+		System.out.println("Hello");
+		System.out.println("Lab 1");
+		System.out.println("1/16/2019");
+		System.out.println("CMSC 255 004");
+		System.out.println();
+		}
+		
+	public static void main(String[] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
+	
